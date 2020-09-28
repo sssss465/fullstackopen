85df670a6766e2f036ed324b7685c4417e1d76e9
Andrew <sssss465@gmail.com>
2020-09-28 01:37:35 -0700

    1-1 to 1-2

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (<h1> {props.course}</h1>);
+}
+const Content = (props) => {
+  return (
+    <div>
+      {
+        props.parts.map((value) => {return <Part part={value[0]} exercise={value[1]}></Part>})
+      }
+    </div>
+  )
+}
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  )
+}
+const Total = (props) => {
+  return (<p>Number of exercises {props.total}</p>)
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}></Header>
+      <Content parts={[[part1, exercises1], [part2, exercises2], [part3, exercises3]]}></Content>
+      <Total total={exercises1 + exercises2 + exercises3}></Total>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
