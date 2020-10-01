6e9fa1e346a2590a0d8279f4c7d0548a90823509
Andrew <sssss465@gmail.com>
2020-10-01 01:43:28 -0700

    2-1 to 2-5

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Total = ({ course }) => {
+  const sum = course.parts.reduce((a, b) => {
+    return a + b.exercises;
+  }, 0);
+  return <p>Number of exercises {sum}</p>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map((part, ind) => (
+        <Part part={part} key={ind} />
+      ))}
+    </div>
+  );
+};
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  );
+};
+export default Course;
