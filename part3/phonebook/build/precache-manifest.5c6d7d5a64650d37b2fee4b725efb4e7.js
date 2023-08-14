b8286010f7f7232cc45f06c2f0daf42ba6f446b6
nobu <1025790+sssss465@users.noreply.github.com>
2023-08-13 23:18:52 -0700

    3.10-3.11

---
+++ part3/phonebook/build/precache-manifest.5c6d7d5a64650d37b2fee4b725efb4e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a606c8e54c9a355ceed3967b29bdbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "420364c407b01352dcf9",
+    "url": "/static/js/2.f3445bfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f3445bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b72b65266e97e9b19227",
+    "url": "/static/js/main.683255ca.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("build"));
 
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
